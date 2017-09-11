99ed9254a123fe23bc864caa1b73b86cd83f4eac
zhouchw3 <zhouchw3@yonyou.com>
2017-09-11 20:58:18 +0800

    ccc

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,10 @@ import "./index.css";
 
 export default class Header extends Component{
 	render(){
-		return <div>
-			<h1 className="head">I am header</h1>
-		</div>;
-	}
+		return(
+			<div className="component-header">
+                <img src="/static/images/logo.png" width="40" alt="" className="-clo-auto"/>
+                <h1 className="caption">React Music Player</h1>
+            </div>
+		)
 }
